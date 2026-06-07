ed7efdfada9112ea7fbfb97a3ceaf1b761007c01
Miladofficial <63480606+Miladofficial@users.noreply.github.com>
2026-06-07 16:05:55 +0330

    Create MainActivity.kt

---
+++ app/src/main/java/com/miladofficial/bankvam/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.miladofficial.bankvam.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.miladofficial.bankvam.presentation.navigation.NavGraph
+import com.miladofficial.bankvam.presentation.ui.theme.BANKVAMTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BANKVAMTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
